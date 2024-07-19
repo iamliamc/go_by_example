8f56a4f5b8ada7ff07131a2aeb3814492f1b5e8c
Liam Considine <liam.considine@gusto.com>
2024-07-19 15:28:07 -0400

    Show how a mutex enables a mutual exclusion lock

---
+++ sync/sync_test.go
@@ -0,0 +1,41 @@
+package sync
+
+import (
+	"sync"
+	"testing"
+)
+
+func TestCounter(t *testing.T) {
+	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
+		counter := Counter{}
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+		assertCounter(t, counter, 3)
+	})
+
+	t.Run("validate it works safely concurrently", func(t *testing.T) {
+		expected := 1000
+		counter := Counter{}
+
+		// sync.WaitGroup which is a convenient way of synchronising concurrent processes.
+		var wg sync.WaitGroup
+		wg.Add(expected)
+
+		for i := 0; i < expected; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+		assertCounter(t, counter, expected)
+	})
+}
+
+func assertCounter(t *testing.T, got Counter, want int) {
+	t.Helper()
+	if got.Value() != want {
+		t.Errorf("got %d, want %d", got.Value(), want)
+	}
+}
