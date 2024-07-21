a88fc916f073c158c62ce03ff4b548537670b499
Liam Considine <ljc3179@gmail.com>
2024-07-21 18:55:56 -0400

    Working through some concepts in the math library for svg clock exercise

--- clockface/clockface.go
+++ clockface/clockface.go
@@ -1,6 +1,9 @@
 package clockface
 
-import "time"
+import (
+	"math"
+	"time"
+)
 
 // A Point represents a two-dimensional Cartesian coordinate
 type Point struct {
@@ -11,5 +14,17 @@ type Point struct {
 // SecondHand is the unit vector of the second hand of an analogue clock at time `t`
 // represented as a Point.
 func SecondHand(t time.Time) Point {
-	return Point{}
+	return Point{150, 60}
+}
+
+func secondsInRadians(t time.Time) float64 {
+	return (math.Pi / (30 / (float64(t.Second()))))
+}
+
+func secondHandPoint(t time.Time) Point {
+	angle := secondsInRadians(t)
+	x := math.Sin(angle)
+	y := math.Cos(angle)
+
+	return Point{x, y}
 }
